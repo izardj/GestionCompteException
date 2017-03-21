1ab3dd88b07b2698d5adfd38a2de8214508a5c94
izardj <izard.jerome@free.fr>
2017-03-21 09:41:54 +0100

    final

---
+++ src/service/CompteService.java
@@ -0,0 +1,50 @@
+package service;
+
+import metier.Compte;
+import service.exceptions.MontantNegatifException;
+import service.exceptions.SoldeInsuffisantException;
+
+/**
+ * Classe permettant de gérer un compte
+ * @author Stagiaire
+ *
+ */
+public class CompteService {
+	/**
+	 * Permet d'ajouter un montant sur un compte
+	 * @param compte Compte à créditer
+	 * @param montant Montant à créditer
+	 * @return Le nouveau solde
+	 */
+	public long ajouter(Compte compte, long montant){
+		if(montant >= 0){
+			compte.setSolde(compte.getSolde() + montant);
+		}
+		return compte.getSolde();
+	}
+	/**
+	 * Permet de retirer un montant sur un compte
+	 * @param compte Compte à débiter
+	 * @param montant montant à débiter
+	 * @return Retourne le  solde
+	 */
+	public long retirer(Compte compte, long montant) throws MontantNegatifException, SoldeInsuffisantException {
+		if(montant >= 0){
+			if(montant <= compte.getSolde()){
+				compte.setSolde(compte.getSolde() - montant);
+			}
+			else throw new SoldeInsuffisantException("SOLDE INSUFFISANT");
+		}
+		else
+			throw new MontantNegatifException("MONTANT NEGATIF");
+		return compte.getSolde();
+	}
+	/**
+	 * Permet de retourner le solde courant d'un compte
+	 * @param compte Compte manipulé
+	 * @return Retourne le solde
+	 */
+	public long solde(Compte compte){
+		return compte.getSolde();
+	}
+}
